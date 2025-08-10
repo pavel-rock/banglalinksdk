1c3fee5f1b13b4e31aa1682dcd8501f2e4cade6f
Masud  Parvez <pavelcep.sust11@gmail.com>
2025-08-10 18:34:45 +0600

    done

--- iscreensdk/src/main/java/com/rockstreamer/iscreensdk/activity/base/BaseActivity.kt
+++ iscreensdk/src/main/java/com/rockstreamer/iscreensdk/activity/base/BaseActivity.kt
@@ -1,8 +1,15 @@
 package com.rockstreamer.iscreensdk.activity.base
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.rockstreamer.iscreensdk.R
 import com.rockstreamer.iscreensdk.adapter.CategoryMainAdapterWithoutAds
 import com.rockstreamer.iscreensdk.api.viewmodel.SliderViewModel
@@ -18,7 +25,7 @@ abstract class BaseActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.statusBarColor = ContextCompat.getColor(this, R.color.iscreen_toolbar_color)
+
         onCreateActivity()
     }
 }
